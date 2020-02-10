f99de9e0dfffa5cb55418b4ba3a3e02185ce5950
Vivek Yadav <vivek.yadav@grofers.com>
2020-02-10 11:22:30 +0530

    version changed to v1.0.16a to v1.0.16b

--- django_prometheus/middleware.py
+++ django_prometheus/middleware.py
@@ -35,7 +35,8 @@ responses_total = Counter(
 requests_latency_before = Histogram(
     'django_http_requests_latency_including_middlewares_seconds',
     ('Histogram of requests processing time (including middleware '
-     'processing time).'))
+     'processing time).'),
+    buckets=DEFAULT_LATENCY_BUCKETS)
 requests_unknown_latency_before = Counter(
     'django_http_requests_unknown_latency_including_middlewares_total',
     ('Count of requests for which the latency was unknown (when computing '
