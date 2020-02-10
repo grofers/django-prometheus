a0ff2229c608246850556a00d5f1a41168e1d1b0
Vivek Yadav <vivek.yadav@grofers.com>
2020-02-10 11:06:39 +0530

    version changed to v1.0.16 to v1.0.16a

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ instructions.
 
 setup(
     name="django-prometheus",
-    version="1.0.16",
+    version="1.0.16a",
     author="Uriel Corfa",
     author_email="uriel@corfa.fr",
     description=(
